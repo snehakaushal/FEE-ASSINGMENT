ca65111ad2146fc0bb513ce4da64fe1929197e64
sneha kaushal <158032357+snehakaushal@users.noreply.github.com>
2024-09-26 22:16:45 +0530

    js-lecture3operators

--- javascript/file1.js
+++ javascript/file1.js
@@ -1,2 +1,45 @@
+let a = 10;
+let b = 5;
 
-console.log("JavaScript linking to html");
+let add = a + b;
+let sub= a - b;
+let mul = a * b;
+let div= a / b;
+let mod = a % b;
+
+console.log("sum:", add);
+console.log(sub:", sub);
+console.log("multipliply:", mul);
+console.log("division:", div);
+console.log("modulus:", mod);
+
+let x = 15;
+x += 5;
+x -= 3;
+x *= 2;
+x /= 2;
+
+console.log(" x:", x);
+
+let isEqual = (a == b);
+let isStrictEqual = (a === b);
+let isNotEqual = (a != b);
+let isGreater = (a > b);
+let isLess = (a < b);
+
+console.log("Is Equal:", isEqual);
+console.log("Is Strictly Equal:", isStrictEqual);
+console.log("Is Not Equal:", isNotEqual);
+console.log("Is Greater:", isGreater);
+console.log("Is Less:", isLess);
+
+let c = true;
+let d = false;
+
+let andOperator = c && d;
+let orOperator = c || d;
+let notOperator = !c;
+
+console.log("AND :", andOperator);
+console.log("OR :", orOperator);
+console.log("NOT:", notOperator);
