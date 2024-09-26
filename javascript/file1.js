571c9cc731558215bf99dc2b5fb311544a2ff8ef
sneha kaushal <158032357+snehakaushal@users.noreply.github.com>
2024-09-26 22:31:16 +0530

    js-lecture10-array methods

--- javascript/file1.js
+++ javascript/file1.js
@@ -1,2 +1,23 @@
-let fruits = ["Apple", "Banana", "Cherry", "Date"];
-console.log("Fruits:", fruits);
+let fruits = ["Sneha", "Lovisha", "Maya"];
+
+fruits.push("Anna");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.shift();
+console.log(fruits);
+
+fruits.unshift("Zara");
+console.log(fruits);
+
+let sliced = fruits.slice(0, 2);
+console.log(sliced);
+
+let index = fruits.indexOf("Lovisha");
+console.log(index);
+
+fruits.forEach((fruit) => {
+    console.log(fruit);
+});
