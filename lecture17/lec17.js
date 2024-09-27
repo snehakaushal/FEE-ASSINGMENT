513c607b10ceb98a4d6bfcca1c5c0c31da8c02c6
sneha kaushal <158032357+snehakaushal@users.noreply.github.com>
2024-09-27 13:15:43 +0530

    js-stringmethods

---
+++ lecture17/lec17.js
@@ -0,0 +1,9 @@
+let name1 = "Sneha", name2 = "Lovisha";
+
+console.log(name1.length, name2.length);
+console.log(name1.toUpperCase(), name2.toUpperCase());
+console.log(name1.toLowerCase(), name2.toLowerCase());
+console.log(name1.concat(" ", name2));
+console.log(name1.substring(0, 3), name2.substring(2, 5));
+console.log(name1.indexOf('e'), name2.indexOf('i'));
+console.log(name1.replace("Sneha", "Sneha Sharma"));
